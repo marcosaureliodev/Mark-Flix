b4ce1f12b34233109549573a3c80788e0e960d53
MARCOS AUÉLIO ALVES DOS REIS <marcosaurlio89@hotmail.com>
2020-08-03 23:49:55 -0300

    Criando cadastro

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+
+  console.log('[nomeDaCategoria]', nomeDaCategoria);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
-        
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
+      <form>
+        {/* State */}
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
 
       <Link to="/">
-          Ir para home
+        Ir para home
       </Link>
     </PageDefault>
   )
